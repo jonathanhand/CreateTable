dc0e4b0fb484a54965b4ff5f10947c5ec99898cf
Jonathan Hand <33673845+jonathanhand@users.noreply.github.com>
2019-10-04 15:38:15 -0700

    changed priceSplit to regex

--- main.js
+++ main.js
@@ -77,7 +77,7 @@ function createTable(rowData) {
         columnArray[rowNum][2] = columnArray[rowNum][2].join(' ')
         var td= document.createElement('td')
         if (cell == 5) {
-          let priceSplit = String(columnArray[rowNum][cell]).split(' ')
+          let priceSplit = String(columnArray[rowNum][cell]).split(/\s+/g)
           console.log(priceSplit)
           if (priceSplit[1] == 'EA') {
             priceSplit[1] = 'each'
